2808750807c2d42cc9d2252570b4cab925fbceaa
JacksonWong <jackson.cw.wong@gmail.com>
2016-02-11 20:36:53 -0500

    testing form submit and whether it's working, also added event.preventDefault() to prevent page from refrehsing

--- js/app.js
+++ js/app.js
@@ -2,9 +2,13 @@ $(document).ready(function(){
 	console.log("Savings Calcuator App loaded");
 
 	// step 1 fetch data from user through form
-	// $('form').submit(
-	
-	// )
+	$('form').submit(
+		function(event){
+			console.log("You've submitted");
+
+			event.preventDefault();
+		}
+	)
 	// step 2 process the data
 
 	// step 3 return the results to the front-end
