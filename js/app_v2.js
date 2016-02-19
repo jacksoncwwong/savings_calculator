e7f87c18f91a707b21e801a1f19bf2ac8aac9a76
JacksonWong <jackson.cw.wong@gmail.com>
2016-02-18 21:28:53 -0500

    added some comments explaining how we're grabbing the data

--- js/app_v2.js
+++ js/app_v2.js
@@ -12,6 +12,16 @@ var formController = function($scope){
 	// and after we write this function we have to call it in our html
 	$scope.getFormData = function(){
 
+
+		// so here it's important that $scope.whatever, that the "whatever"
+		// part is the same as your ng-model="whatever" in your html
+		// so you'll see here that my $scope.whatever always ends with an Ang
+		// and that's because in my html they all end with an Ang
+		// but the "key" for an object here, can be whatever I want
+		// the only thing is when I call on that property later I have to remember what I named it
+		// so it makes more sense to make them relatively similar
+		// so what I mean is if I was to find out what the savings goal is
+		// I would call formData.savingsGoal, not formData.savingsGoalAng
 		var formData = {
 			'savingsGoal': $scope.savingsGoalAng,
 			'goalMonth': $scope.goalMonthAng,
